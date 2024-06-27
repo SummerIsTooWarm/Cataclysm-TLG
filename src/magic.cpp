e70ecf108769cc6df46d8c4db202a432a6a54a79
Standing-Storm <120433252+Standing-Storm@users.noreply.github.com>
2024-06-27 16:18:31 -0500

    Fix concentration metamagic sign flipping (#74810)
    * Initial commit
    
    * formatting
    
    * Update src/magic.cpp
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Kick tests
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/magic.cpp
+++ src/magic.cpp
@@ -1427,6 +1427,9 @@ float spell::spell_fail( const Character &guy ) const
         }
         float concentration_loss = ( 1.0f - ( guy.get_focus() / 100.0f ) ) *
                                    temp_concentration_difficulty_multiplyer;
+        if( concentration_loss >= 1.0f ) {
+            return 1.0f;
+        }
         fail_chance /= 1.0f - concentration_loss;
         psi_fail_chance /= 1.0f - concentration_loss;
     }
